24018c296217c455184e664d5091b68781c4aea1
Mohit Seth <mseth1@jcp.com>
2019-09-08 21:44:33 -0500

    Updates

---
+++ precache-manifest.3e9c08d6b10440f72784402252da3800.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2d249e7f67641bcb4248ffe10a26241",
+    "url": "/flickr-gallery/index.html"
+  },
+  {
+    "revision": "a77cff7af8a5ced34727",
+    "url": "/flickr-gallery/static/css/2.7fad5d03.chunk.css"
+  },
+  {
+    "revision": "68a9197c6b253282ac1d",
+    "url": "/flickr-gallery/static/css/main.78db6309.chunk.css"
+  },
+  {
+    "revision": "a77cff7af8a5ced34727",
+    "url": "/flickr-gallery/static/js/2.3c8ff772.chunk.js"
+  },
+  {
+    "revision": "68a9197c6b253282ac1d",
+    "url": "/flickr-gallery/static/js/main.8e8f8c7e.chunk.js"
+  },
+  {
+    "revision": "ee65c24f4c2a6ea0b889",
+    "url": "/flickr-gallery/static/js/runtime~main.43a184ad.js"
+  },
+  {
+    "revision": "6f0a36b98b8677ddf9421ca507dcb376",
+    "url": "/flickr-gallery/static/media/close.6f0a36b9.svg"
+  },
+  {
+    "revision": "f2c88a640bb87b81555b4d2e73f9a406",
+    "url": "/flickr-gallery/static/media/left-chevron.f2c88a64.svg"
+  },
+  {
+    "revision": "a1f3597033386b21a95567a4b546194e",
+    "url": "/flickr-gallery/static/media/right-chevron.a1f35970.svg"
+  }
+]);
